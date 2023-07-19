181a6076e363a60688e15bc18c4ccf542c6b2308
semlee <92006805+semlee@users.noreply.github.com>
2023-07-20 01:14:42 +0900

    Create IntSoftmax.c

---
+++ IntSoftmax.c
@@ -0,0 +1,90 @@
+//#include "softmax.h"
+#define SIZE 8
+
+void IntSoftmax(int m1[SIZE], int m2[SIZE], int sftmx[SIZE]) {
+    float coef[3] = [0.35815147, 0.96963238, 1.]; //[a, b, c] --> ax^2 + bx + c
+    float x0 = -0.6931 // ln2
+    
+}
+class IntSoftmax(Module):
+    """
+    Class to quantize given Softmax layer
+
+    Parameters:
+    ----------
+    output_bit : int
+        Bitwidth for the Softmax output.
+    quant_mode : 'none' or 'symmetric', default 'none'
+        The mode for quantization. 'none' for no quantization.
+    force_dequant : str, default 'none'
+        Force dequantize Softmax if either 'softmax' or 'nonlinear' is given.
+    """
+    def __init__(self,
+                 output_bit,
+                 quant_mode='none',
+                 force_dequant='none'):
+        super(IntSoftmax, self).__init__()
+        self.output_bit = output_bit
+        self.quant_mode = quant_mode
+        if force_dequant in ['nonlinear', 'softmax']:
+            logger.info("Force dequantize softmax")
+            self.quant_mode = 'none'
+
+
+        self.act = QuantAct(16, quant_mode=self.quant_mode)
+        self.x0 = -0.6931 # -ln2
+        self.n = 30 # sufficiently large integer
+        self.coef = [0.35815147, 0.96963238, 1.] //[a, b, c] --> ax^2 + bx + c
+        self.coef[1] /= self.coef[0]
+        self.coef[2] /= self.coef[0]
+
+    def fix(self):
+        pass
+
+    def unfix(self):
+        pass
+
+    def int_polynomial(self, x_int, scaling_factor):
+        with torch.no_grad():
+            b_int = torch.floor(self.coef[1] / scaling_factor)
+            c_int = torch.floor(self.coef[2] / scaling_factor ** 2)
+        z = x_int + b_int
+        z = x_int * z
+        z = z + c_int
+        scaling_factor = self.coef[0] * scaling_factor ** 2
+        return z, scaling_factor
+
+    def int_exp(self, x_int, scaling_factor):
+        with torch.no_grad():
+            x0_int = torch.floor(self.x0 / scaling_factor)
+        x_int = torch.max(x_int, self.n * x0_int)
+
+        q = floor_ste.apply(x_int / x0_int)
+        r = x_int - x0_int * q
+        exp_int, exp_scaling_factor = self.int_polynomial(r, scaling_factor)
+        exp_int = torch.clamp(floor_ste.apply(exp_int * 2 ** (self.n - q)), min=0)
+        scaling_factor = exp_scaling_factor / 2 ** self.n
+        return exp_int, scaling_factor
+
+    def forward(self, x, scaling_factor):
+        if self.quant_mode == 'none':
+            return utils.softmax(x, dim=-1, onnx_trace=False), None
+
+        assert self.quant_mode == 'symmetric', \
+                "unsupported quant mode: {}".format(quant_mode)
+
+        x_int = x / scaling_factor
+
+        x_int_max, _ = x_int.max(dim=-1, keepdim=True)
+        x_int = x_int - x_int_max
+
+
+        exp_int, exp_scaling_factor = self.int_exp(x_int, scaling_factor)
+        exp, exp_scaling_factor = self.act(exp_int, exp_scaling_factor)
+        exp_int = exp / exp_scaling_factor
+        exp_int_sum = exp_int.sum(dim=-1, keepdim=True)
+
+        factor = floor_ste.apply(2**32 / exp_int_sum)
+        exp_int = floor_ste.apply(exp_int * factor / 2 ** (32 - self.output_bit))
+        scaling_factor = 1 / 2 ** self.output_bit
+        return exp_int * scaling_factor, scaling_factor
